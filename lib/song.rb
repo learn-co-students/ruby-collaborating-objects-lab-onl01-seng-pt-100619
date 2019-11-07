362a994436d6bf906aec14073d60f55132eaa487
allison cortez <allizetorc@gmail.com>
2019-11-07 05:19:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,32 @@
 class Song
-  attr_accessor :name,:artist
+  attr_accessor :name, :artist, :artist_name
   @@all = []
   def initialize(name)
     @name = name
     save
   end
+  
   def save
     @@all << self
   end
+  
   def self.all
     @@all
   end
+  
   def self.new_by_filename(filename)
-    split_file = filename.split(" - ")
-    song_name = split_file[1] #I think it was 1, going by memory though
+    split_files = filename.split(" - ")
+    song_name = split_files[1]
     new_song = self.new(song_name)
     #now do the same with artist
     #and think about associations
     #also think about what they want you to return at the end
   end
+  
+  def self.artist_name(artist_name)
+    split_files = filename.split(" - ")
+    artist_name = split_files[1]
+    new_artist = self.new(artist_name)
+  end
+  
 end
